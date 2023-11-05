856cff92ceaf0760e23cbd2552de6fc7e43e02b6
KobilyanskyS <kobilyansky.s@gmail.com>
2023-11-05 21:32:13 +0300

    conflicts

--- my_notes_handler.php
+++ my_notes_handler.php
@@ -10,7 +10,7 @@ if (isset($_COOKIE["username"])) {
     $end_date = date("Y-m-d", strtotime("+6 days", strtotime($current_date)));
 
 // SQL-запрос для выборки занятых записей в заданном диапазоне дат
-$sql = "SELECT n.date, m.name
+$sql = "SELECT n.date, m.name, , m.is_active
 FROM notes AS n
 JOIN machines AS m ON n.machine_id = m.id WHERE n.user_id = :user_id and n.date BETWEEN :current_date AND :end_date ORDER BY n.date ASC";
 $stmt = $pdo->prepare($sql);
