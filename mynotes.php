249e758deb3631001995fbe5ccd1b4aec238555a
KobilyanskyS <kobilyansky.s@gmail.com>
2023-11-05 21:36:37 +0300

    fix small bugs

--- mynotes.php
+++ mynotes.php
@@ -23,7 +23,7 @@
 
 
 
-<script src="js/bootstrap.bundle.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery.min.js"></script>
 <script src="js/myNotesAjax.js"></script>
 </body>
